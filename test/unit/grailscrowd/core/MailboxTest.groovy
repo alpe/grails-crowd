30c4b0b95e62be157efdf4474c2831b34d927977
alpe <info@alexanderpeters.de>
2008-10-19 13:54:17 +0200

    + tests

--- test/unit/grailscrowd/core/MailboxTest.groovy
+++ test/unit/grailscrowd/core/MailboxTest.groovy
@@ -18,21 +18,41 @@ class MailboxTest extends AbstractBaseUnitTestCase{
         super.setUp()
         mailbox = null
         fixi = new MailboxFixture()
+        mailbox = fixi.sampleBox
     }
 
     void testHasAnyNewMessages_3new_true(){
-        mailbox = fixi.sampleBox
         assertThat(mailbox, is(notNullValue()))
         assertTrue mailbox.hasAnyNewMessages()
     }
 
     void testGetInboxMessageAndMarkAsSeen_validId_mesageNotNullAndStatusSeen(){
-        mailbox = fixi.sampleBox
         def msg = mailbox.inboxMessages.find{it}
         msg.setId(1)
         def message = mailbox.getInboxMessageAndMarkAsSeen(1)
         assertThat(message, is(msg))
         assertThat(message.status, is(MessageLifecycle.SEEN))
     }
+
+
+    void testVisibleInInbox_validSample_true(){
+        def msg =[isDeleted:{false}, sentDate: new Date()]
+        assertTrue(mailbox.visibleInInbox.call(msg))
+        Calendar cal = GregorianCalendar.getInstance()
+        cal.add(Calendar.DAY_OF_MONTH, (Mailbox.MAX_DAYS_VISIBILITY)*-1)
+        cal.add(Calendar.SECOND, 1)
+        msg =[isDeleted:{false}, sentDate: cal.time]
+        assertThat(mailbox.visibleInInbox.call(msg), is(true))
+    }
+
+    void testVisibleInInbox_deletedStatus_false(){
+        def msg =[isDeleted:{true}, sentDate: new Date()]
+        assertThat(mailbox.visibleInInbox.call(msg), is(false))
+    }
+    void testVisibleInInbox_expiredSentDate_false(){
+        def msg =[isDeleted:{false}, sentDate: new Date()-Mailbox.MAX_DAYS_VISIBILITY]
+        assertThat(mailbox.visibleInInbox.call(msg), is(false))
+    }
+
 
 }
