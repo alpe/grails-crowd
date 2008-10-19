f4d4b67218d1c562497df4146765641e4aaf95e5
alpe <info@alexanderpeters.de>
2008-10-19 14:45:10 +0200

    better subject testing support

--- test/unit/grailscrowd/core/message/GenericMessageTest.groovy
+++ test/unit/grailscrowd/core/message/GenericMessageTest.groovy
@@ -2,6 +2,9 @@ package grailscrowd.core.message
 
 import grailscrowd.core.*
 import grailscrowd.util.*
+import static org.hamcrest.CoreMatchers.*
+import static org.junit.Assert.assertThat
+
 
 /**
  * @author ap
@@ -10,6 +13,12 @@ class GenericMessageTest extends AbstractBaseUnitTestCase{
 
     def message
 
+
+    void setUp(){
+        super.setUp()
+        message = MessageFixture.anyFreeFormMessage
+    }
+
     void testIsSystemMessage_withInvitationMessage_expectTrue(){
         message = MessageFixture.anyInviationMessage
         assertTrue(message.isSystemMessage())
@@ -17,9 +26,40 @@ class GenericMessageTest extends AbstractBaseUnitTestCase{
 
 
     void testIsSystemMessage_withFreeFormMessage_expectFalse(){
-            message = MessageFixture.anyFreeFormMessage
             assertFalse(message.isSystemMessage())
         }
+
+    void testIsDeleted_notDelStatus_false(){
+        for (status in MessageLifecycle.values()){
+            if (status == MessageLifecycle.DELETED){ continue}
+            message.status=status
+            assertFalse(message.isDeleted())
+        }
+    }
+    void testIsDeleted_delStatus_true(){
+        message.status=MessageLifecycle.DELETED
+        assertTrue(message.isDeleted())
+    }
+
+    void testCompareTo_samples_orderedAsExpected(){
+        message = MessageFixture.anyFreeFormMessage
+        message.sentDate = new Date()
+        def message2 = MessageFixture.anyInviationMessage
+        message2.sentDate = new Date()-10
+        def message3 = MessageFixture.anyFreeFormMessage
+        message3.sentDate = new Date()-5
+        def sortedList = [message, message2, message3].sort()
+        assertThat("recieved: "+sortedList*.sentDate, sortedList, is ([message2, message3, message]))
+    }
+
+    void testGetSubject_freeForm_subjectAsExpected(){
+        assertThat( message.subject, is (MessageFixture.ANY_FREEFORM_SUBJECT))
+    }
+    void testGetSubject_invitationMessage_subjectAsExpected(){
+        message = MessageFixture.anyInviationMessage
+        assertThat('System message subjects are in message.property. Plesase look for modifications.',
+                message.subject, is (MessageFixture.ANYINVITATION_SUBJECT))
+    }
 
 
 }
--- test/unit/grailscrowd/core/message/MessageFixture.groovy
+++ test/unit/grailscrowd/core/message/MessageFixture.groovy
@@ -8,16 +8,24 @@ import grailscrowd.util.MockUtils
  */
 class MessageFixture {
 
+    static String ANY_FREEFORM_SUBJECT = 'Testmail'
+    static String ANYINVITATION_SUBJECT = 'Project Invitation'
+
+    /** fake message.property access  */
+    static def createFakeMessages(def messageResult){
+        [getMessage:{a,b,c,d->messageResult}]
+    }
 
     static GenericMessage getAnyInviationMessage(){
         def anySender = MemberFixture.ottoOne
         def anyProject = GrailsProjectFixture.grailscrowdSample
         def result = SystemMessageFactory.createInvitation(anySender, anyProject)
+        result.payload.messageSource = createFakeMessages(ANYINVITATION_SUBJECT)
         MockUtils.mockDomain(result)
         return result
     }
 
-    static GenericMessage getAnyFreeFormMessage(def subject ='Testmail'){
+    static GenericMessage getAnyFreeFormMessage(def subject =ANY_FREEFORM_SUBJECT){
         def result = new GenericMessage()
         result.setPayload(new FreeFormMessagePayload(subject:subject, body:"Hello World,\nthis is a test message.\n with subject: $subject"))
         MockUtils.mockDomain(result)        
