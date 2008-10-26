1face2d08b8949a7710c5299502fb68e83a35737
alpe <info@alexanderpeters.de>
2008-10-26 23:07:34 +0100

    + mailbody, blacklist, mailsending

--- grails-app/jobs/MailerJob.groovy
+++ grails-app/jobs/MailerJob.groovy
@@ -3,24 +3,69 @@ import grailscrowd.core.message.*
 
 
 /**
- * Notification job like in grails.org.
+ * Quartz job running every minute to poll new messages from notification FIFO
+ * and send as info via email like in grails.org.
  * @author ap
  */
 class MailerJob {
-    
+
+    /**
+     * To prevent flooding test accounts, any outgoing mailaddress must not
+     * be in this regExp blacklist.
+     */
+    static final List BLACKLIST =[ ".*@example.com", "gash@gmx.de"]    
+
+
     def startDelay = 6000
     def timeout = 60000     // execute job every minute
 
     MessageNotificationFIFO messageNotificationFIFO
-//    def mailService
-//    def cacheService
-    
+
+    def mailService
+
+
     def execute() {
         def message
         while((message = messageNotificationFIFO?.pollOffStack())) {
-            message.getRecipients().grep{it.canBeNotifiedViaEmail}.each{recipient->
-                println "Sending notification '${message.subject}' to: "+recipient
+            if (!message.isNew()){ return}
+            message.getRecipients().grep{
+                it.canBeNotifiedViaEmail && !isInBlacklist(it)
+            }.each{recipient->
+                mailService?.sendMail {
+                                     title "New message in your Grailscrowd mailbox."
+                                     from "noreply@grailscrowd.com"
+                                     replyTo "noreply@grailscrowd.com"
+                                     to recipient.email
+                                     text createNotificationMessage(recipient, message)
+                                 }
             }
         }
 	}
+
+    /** Is given recipient in blacklist?*/
+    public boolean isInBlacklist(recipient){
+        return BLACKLIST.any{recipient.email ==~it}
+    }
+
+    /**
+     * Create text of mailbody.
+     * @return text
+     */
+    def createNotificationMessage = {recipient, message->
+        StringBuilder sb = new StringBuilder()
+        sb<<"Hi ${recipient.displayName},\n\n"
+        sb<<"you have recieved a new message from ${message.sender.displayName} with subject:\n"
+        sb<<"\n"        
+        sb<<"'${message.subject}'\n"
+        sb<<"\n"
+        sb<<"Visit it at: http://www.grailscrowd.com/grailscrowd/mailbox/inbox\n"
+        sb<<"\n"
+        sb<<"Regards,\n"
+        sb<<"Your GC Email Monkey\n"
+        sb<<"-- \n"
+        sb<<"You can change your mail and notification settings at:\n"
+        sb<<"http://www:grailscrowd.com/grailscrowd/account\n"
+        return sb.toString()
+    }
+
 }
