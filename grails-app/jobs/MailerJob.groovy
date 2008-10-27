1d02f0fa94c43da91aabdf6dcea7f1dd27e2dee6
alpe <info@alexanderpeters.de>
2008-10-28 00:07:21 +0100

    + external config and statistic

--- grails-app/jobs/MailerJob.groovy
+++ grails-app/jobs/MailerJob.groovy
@@ -1,4 +1,3 @@
-
 import grailscrowd.core.message.*
 
 
@@ -9,62 +8,81 @@ import grailscrowd.core.message.*
  */
 class MailerJob {
 
-    /**
-     * To prevent flooding test accounts, any outgoing mailaddress must not
-     * be in this regExp blacklist.
-     */
-    static final List BLACKLIST =[ ".*@example.com", "gash@gmx.de"]    
-
-
-    def startDelay = 6000
-    def timeout = 60000     // execute job every minute
-
+    /** notification fifo    */
     MessageNotificationFIFO messageNotificationFIFO
 
+    def mailerJobConfig
+
+    /** blacklist impl    */
+    def blacklist
+
+    /** plugin mail service    */
     def mailService
 
+    /** job statistics    */
+    def mailerJobStatistics
 
+    def mailCircuitBreaker
+
+    def getStartDelay(){
+        return mailerJobConfig?.startDelay
+    }
+    def getTimeout(){
+        return mailerJobConfig?.timeout
+    }
+
+    /** Job jump in method.
+     */
     def execute() {
-        def message
-        while((message = messageNotificationFIFO?.pollOffStack())) {
-            if (!message.isNew()){ return}
-            message.getRecipients().grep{
-                it.canBeNotifiedViaEmail && !isInBlacklist(it)
-            }.each{recipient->
-                mailService?.sendMail {
-                                     title "New message in your Grailscrowd mailbox."
-                                     from "noreply@grailscrowd.com"
-                                     replyTo "noreply@grailscrowd.com"
-                                     to recipient.email
-                                     text createNotificationMessage(recipient, message)
-                                 }
+        if (!mailerJobConfig?.enabled){ return }
+        mailerJobStatistics?.markJobStarted()
+        try {
+            // execute in fail fast circuit breaker
+            mailCircuitBreaker.execute {
+                def message
+                while ((message = messageNotificationFIFO?.pollOffStack())) {
+                    if (!message.isNew()) { return }
+                    message.getRecipients().grep {
+                        it.canBeNotifiedViaEmail && !isInBlacklist(it)
+                    }.each {recipient ->
+                        mailService?.sendMail {
+                            title "New message in your Grailscrowd mailbox."
+                            from "noreply@grailscrowd.com"
+                            replyTo "noreply@grailscrowd.com"
+                            to recipient.email
+                            text createNotificationMessage(recipient, message)
+                        }
+                    }
+                }
             }
+        } finally {
+            mailerJobStatistics?.markJobEnded()
         }
-	}
+    }
 
-    /** Is given recipient in blacklist?*/
-    public boolean isInBlacklist(recipient){
-        return BLACKLIST.any{recipient.email ==~it}
+    /** Is given recipient in blacklist?   */
+    public boolean isInBlacklist(recipient) {
+        return blacklist.isInBlacklist(recipient.email)
     }
 
     /**
      * Create text of mailbody.
      * @return text
      */
-    def createNotificationMessage = {recipient, message->
+    def createNotificationMessage = {recipient, message ->
         StringBuilder sb = new StringBuilder()
-        sb<<"Hi ${recipient.displayName},\n\n"
-        sb<<"you have recieved a new message from ${message.sender.displayName} with subject:\n"
-        sb<<"\n"        
-        sb<<"'${message.subject}'\n"
-        sb<<"\n"
-        sb<<"Visit it at: http://www.grailscrowd.com/grailscrowd/mailbox/inbox\n"
-        sb<<"\n"
-        sb<<"Regards,\n"
-        sb<<"Your GC Email Monkey\n"
-        sb<<"-- \n"
-        sb<<"You can change your mail and notification settings at:\n"
-        sb<<"http://www:grailscrowd.com/grailscrowd/account\n"
+        sb << "Hi ${recipient.displayName},\n\n"
+        sb << "you have recieved a new message from ${message.sender.displayName} with subject:\n"
+        sb << "\n"
+        sb << "'${message.subject}'\n"
+        sb << "\n"
+        sb << "Visit it at: http://www.grailscrowd.com/grailscrowd/mailbox/inbox\n"
+        sb << "\n"
+        sb << "Regards,\n"
+        sb << "Your GC Email Monkey\n"
+        sb << "-- \n"
+        sb << "You can change your mail and notification settings at:\n"
+        sb << "http://www:grailscrowd.com/grailscrowd/account\n"
         return sb.toString()
     }
 
