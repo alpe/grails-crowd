b2475fe1d6d185b1cbc72bcd61ae540739d2fe39
alpe <info@alexanderpeters.de>
2008-10-28 00:07:02 +0100

    + mailerjob configs

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -6,6 +6,10 @@ import org.springframework.jmx.export.assembler.MetadataMBeanInfoAssembler
 import org.springframework.jmx.export.naming.MetadataNamingStrategy
 
 import grailscrowd.core.message.MessageNotificationFIFO
+import grailscrowd.core.message.Blacklist
+import grailscrowd.core.message.MailerJobStatistics
+import grailscrowd.core.message.MailerJobConfig
+import grailscrowd.core.util.CircuitBreaker
 
 
 
@@ -15,11 +19,34 @@ beans = {
 
 
     //-------------------------------------------
-    // grailscrowd beans
+    // Circuit breaker
     //-------------------------------------------
 
+    mailCircuitBreaker(CircuitBreaker){
+        threshold = 12 // failure counts until open
+        resetTime = 8*60*1000 // ms
+    }
+
+
+    //-------------------------------------------
+    // grailscrowd beans
+    //-------------------------------------------
+    mailerJobConfig(MailerJobConfig){
+        startDelay = 6000
+        timeout = 60000     // execute job every minute
+        enabled = true
+    }
+
+    mailerJobStatistics(MailerJobStatistics){
+        // noting to configure
+    }
+
+    blacklist(Blacklist){
+        // noting to configure
+    }
+
     messageNotificationFIFO(MessageNotificationFIFO){
-        // currently noting to configure
+        // noting to configure
     }
 
 
@@ -41,16 +68,15 @@ beans = {
         attributeSource = jmxAttributeSource
     }
 
-    namingStrategy(MetadataNamingStrategy){
-        attributeSource = jmxAttributeSource
-    }
-
     exporter(MBeanExporter){
         server = mbeanServer
         assembler = assembler
-//        namingStrategy = namingStrategy
         beans = ['gc.hibernate:name=hibernate.statistics':hibernateStatistics,
-                 'gc.mail:name=notification.fifo':messageNotificationFIFO
+                 'gc.mail:name=notification.fifo':messageNotificationFIFO,
+                'gc.mail:name=job.settings':mailerJobConfig,
+                'gc.mail:name=job.statistics':mailerJobStatistics,
+                'gc.mail:name=blacklist':blacklist,
+                'gc.mail:name=circuitbreaker.ext':mailCircuitBreaker
         ]
     }
 
