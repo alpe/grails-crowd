28baea5e59c9cbbd0020ac0c6806f325323de5d8
alpe <info@alexanderpeters.de>
2008-10-28 23:58:20 +0100

    configuration for secondary notification queue

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -5,78 +5,93 @@ import org.springframework.jmx.export.annotation.AnnotationJmxAttributeSource
 import org.springframework.jmx.export.assembler.MetadataMBeanInfoAssembler
 import org.springframework.jmx.export.naming.MetadataNamingStrategy
 
-import grailscrowd.core.message.MessageNotificationFIFO
+import grailscrowd.core.message.MessageNotificationQueue
 import grailscrowd.core.message.Blacklist
 import grailscrowd.core.message.MailerJobStatistics
 import grailscrowd.core.message.MailerJobConfig
 import grailscrowd.core.util.CircuitBreaker
 
-
-
-
 // Place your Spring DSL code here
 beans = {
-
 
     //-------------------------------------------
     // Circuit breaker
     //-------------------------------------------
 
-    mailCircuitBreaker(CircuitBreaker){
+    mailCircuitBreaker(CircuitBreaker) {
         threshold = 12 // failure counts until open
-        resetTime = 8*60*1000 // ms
+        resetTime = 8 * 60 * 1000 // ms
     }
 
-    blacklist(Blacklist){
-        blacklistRegExps =[".*@example.com", "gash@gmx.de"]
+    blacklist(Blacklist) {
+        blacklistRegExps = [".*@example.com", "gash@gmx.de"]
     }
-    //-------------------------------------------
-    // grailscrowd beans
-    //-------------------------------------------
-    mailboxMailerJobConfig(MailerJobConfig){
+    //-----------------------------------------------
+    // mailbox notification priority queue job config
+    //-----------------------------------------------
+    mailboxMailerJobConfig(MailerJobConfig) {
         startDelay = 6000
         timeout = 60000     // execute job every minute
         enabled = true
     }
 
-    mailboxMailerJobStatistics(MailerJobStatistics){
+    mailboxMailerJobStatistics(MailerJobStatistics) {
         // noting to configure
     }
 
-
-
-    mailboxMessageNotificationFIFO(MessageNotificationFIFO){
+    mailboxMessageNotificationQueue(MessageNotificationQueue) {
         // noting to configure
     }
 
+    //-----------------------------------------------
+    // comment notification queue job config
+    //-----------------------------------------------
+
+    transientMessageNotificationQueue(MessageNotificationQueue) {
+        // noting to configure
+    }
+
+    transientMailerJobConfig(MailerJobConfig) {
+        startDelay = 6000
+        timeout = 20000     // execute job every 2 minutes
+        enabled = true
+    }
+
+    transientMailerJobStatistics(MailerJobStatistics) {
+        // noting to configure
+    }
 
     //-------------------------------------------
     // jmx stuff
     //-------------------------------------------
 
-    mbeanServer(MBeanServerFactoryBean){
-           locateExistingServerIfPossible = true
-       }
-    hibernateStatistics(StatisticsService){sessionFactory->
+    mbeanServer(MBeanServerFactoryBean) {
+        locateExistingServerIfPossible = true
+    }
+    hibernateStatistics(StatisticsService) {sessionFactory ->
         statisticsEnabled = true
         this.sessionFactory = sessionFactory
     }
 
-    jmxAttributeSource(AnnotationJmxAttributeSource){}
+    jmxAttributeSource(AnnotationJmxAttributeSource) {}
 
-    assembler(MetadataMBeanInfoAssembler){
+    assembler(MetadataMBeanInfoAssembler) {
         attributeSource = jmxAttributeSource
     }
 
-    exporter(MBeanExporter){
+    exporter(MBeanExporter) {
         server = mbeanServer
         assembler = assembler
-        beans = ['gc.hibernate:name=hibernate.statistics':hibernateStatistics,
-                 'gc.mail:name=mailbox.notification.fifo':mailboxMessageNotificationFIFO,
-                 'gc.mail:name=mailboxjob.settings':mailboxMailerJobConfig,
-                 'gc.mail:name=mailboxjob.statistics':mailboxMailerJobStatistics,
-                 'gc.mail:name=blacklist':blacklist,
-                 'gc.mail:name=circuitbreaker.ext':mailCircuitBreaker
+        beans = ['gc.hibernate:name=hibernate.statistics': hibernateStatistics,
+                'gc.mail:name=mailbox.notificationQueue': mailboxMessageNotificationQueue,
+                'gc.mail:name=mailboxJob.settings': mailboxMailerJobConfig,
+                'gc.mail:name=mailboxJob.statistics': mailboxMailerJobStatistics,
+                'gc.mail:name=blacklist': blacklist,
+                'gc.mail:name=circuitbreaker.ext': mailCircuitBreaker,
+                'gc.mail:name=secondary.notificationQueue': transientMessageNotificationQueue,
+                'gc.mail:name=secondaryJob.settings': transientMailerJobConfig,
+                'gc.mail:name=secondaryJob.statistics': transientMailerJobStatistics,
+
         ]
     }
 
