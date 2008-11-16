68796753c079e62eede779fdaf2ab85625e4a279
alpe <info@alexanderpeters.de>
2008-11-16 21:10:59 +0100

    minor refactoring

--- test/integration/grailscrowd/core/message/MessageTestUtils.groovy
+++ test/integration/grailscrowd/core/message/MessageTestUtils.groovy
@@ -5,25 +5,38 @@ package grailscrowd.core.message
 class MessageTestUtils {
 
 
-    static def createMessages= {threadQuantity=1, int msgQuantity = 1->
-          def anySubject = "anySubject"
-          def anyBody = "anyBody"
-          def result = []
-          ConversationThread.withTransaction{ctx->
-              threadQuantity.times{index->
-                  def message = FreeFormMessageFactory.createNewMessage(anySender, anyBody+"$index 1/$msgQuantity")
-                  messageService.startNewConversation( anySubject,  anySender,  anyRecipient, message)
-                  (msgQuantity-1).times{
-                      def responseMsg = FreeFormMessageFactory.createNewMessage(anySender, anyBody+"$index ${1+it}/$msgQuantity")
-                      messageService.respondToMessage(message,anySender, responseMsg)
-                  }
+    static def createMessages = {threadQuantity = 1, int msgQuantity = 1 ->
+        def anySubject = "anySubject"
+        def anyBody = "anyBody"
+        def result = []
+        ConversationThread.withTransaction {ctx ->
+            threadQuantity.times {index ->
+                def message = FreeFormMessageFactory.createNewMessage(anySender, anyBody + "$index 1/$msgQuantity")
+                assert message
+                messageService.startNewConversation(anySubject, anySender, anyRecipient, message)
+                result << message
+                def addRespClosure = MessageTestUtils.addRespMessage
+                addRespClosure.delegate = delegate
+                result.addAll addRespClosure((msgQuantity - 1), message)
+                anyRecipient.save(flush: true)
+                assert message.id                
+            }
+        }
+        return result
+    }
 
-                  anyRecipient.save(flush:true)
-                  assert message.id
-                  result<<message
-              }
-          }
-          return result
-      }
+    static def addRespMessage = {int msgQuantity, message ->
+        def anyBody = "anyRespBody"
+        def result = []
+        ConversationThread.withTransaction {ctx ->
+            msgQuantity.times {
+                def responseMsg = FreeFormMessageFactory.createNewMessage(anySender, anyBody + " ${1 + it}/$msgQuantity")
+                messageService.respondToMessage(message, anySender, responseMsg)
+                result << responseMsg                
+            }
+        }
+        return result
+    }
+
 
 }
