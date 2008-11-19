b579d38386b6b2d0e2b2b3ecde1d81efb15c70bd
alpe <info@alexanderpeters.de>
2008-11-19 13:56:38 +0100

    moved to filter package

---
+++ grails-app/conf/filter/LoggedInMemberFilters.groovy
@@ -0,0 +1,23 @@
+package filter
+
+
+import grailscrowd.core.*
+
+class LoggedInMemberFilters {
+
+    def filters = {
+        exposeLoggedInMember(controller: '*', action: '*') {
+            after = {model ->
+                if (session.memberId) {
+                    def loggedInMember = Member.get(session.memberId)
+                    if(model) {
+                        model.loggedInMember = loggedInMember
+                    }
+                    else {
+                        model = [loggedInMember:loggedInMember]
+                    }
+                }
+            }
+        }
+    }
+}
